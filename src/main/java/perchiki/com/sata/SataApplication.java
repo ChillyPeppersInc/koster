67d4455529c7469bf5c17f580d038c5da8d0e35d
Kotelnikov Anton <aokotelnikov@edu.hse.ru>
2025-04-09 14:55:54 +0300

    Clear project

---
+++ src/main/java/perchiki/com/sata/SataApplication.java
@@ -0,0 +1,13 @@
+package perchiki.com.sata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/perchiki/com/sata/SataApplicationTests.java
@@ -0,0 +1,13 @@
+package perchiki.com.sata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
