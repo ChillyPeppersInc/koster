7ca684663af10b0d171436644fa9b559c22714db
Arslan764 <arslanhismatullin00@mail.ru>
2025-05-27 20:49:07 +0300

    login v1
    +LoginService
    +LoginDTO
    LoginController updated

--- src/main/java/ru/ChillyPeppersInc/koster/controllers/LoginController.java
+++ src/main/java/ru/ChillyPeppersInc/koster/controllers/LoginController.java
@@ -1,13 +1,39 @@
-//package ru.ChillyPeppersInc.koster.controllers;
-//
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//
-//@Controller
-//public class LoginController {
-//    @GetMapping("/login")
-//    public String singUp() {
-//        return "login.html";
-//    }
-//}
+package ru.ChillyPeppersInc.koster.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.ChillyPeppersInc.koster.Services.LoginService;
+import ru.ChillyPeppersInc.koster.dto.LoginDTO;
+
+@Controller
+public class LoginController {
+    private final LoginService loginService;
+
+    public LoginController(LoginService loginService) {
+        this.loginService = loginService;
+    }
+
+    @GetMapping("/login")
+    public String singUp() {
+        return "login.html";
+    }
+
+    @PostMapping("/login")
+    public String login(LoginDTO loginDTO, Model model) {
+        boolean rightPassword = loginService.checkpassword(
+                loginDTO.getMail(),
+                loginDTO.getPassword()
+        );
+
+        if (rightPassword) {
+            return "redirect:/";
+        } else {
+            model.addAttribute("error", "Invalid username or password.");
+            return "login.html";
+        }
+
+    }
+}
---
+++ src/main/java/ru/ChillyPeppersInc/koster/dto/LoginDTO.java
@@ -0,0 +1,11 @@
+package ru.ChillyPeppersInc.koster.dto;
+
+import lombok.Data;
+
+
+@Data
+public class LoginDTO {
+    private String mail;
+    private String password;
+}
+
