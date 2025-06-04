e158bace2d90bb793e2895cb3bac4cdb025c2939
GafrikJr <gafrikjr22848@gmail.com>
2025-06-04 15:08:21 +0300

    completed entities

---
+++ src/main/java/ru/ChillyPeppersInc/koster/models/Subscriber.java
@@ -0,0 +1,115 @@
+package ru.ChillyPeppersInc.koster.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Past;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "Friendships")
+public class Subscriber {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @JoinColumn(name = "subscriber_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User subscriber;
+
+    @JoinColumn(name = "user_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @Column(name = "date_of_following")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Past
+    private Date dateOfFollowing;
+
+    @Column(name = "created_at")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Past
+    private Date createdAt;
+
+    @Column(name = "updated_at")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Past
+    private Date updatedAt;
+
+    @Column(name = "deleted_at")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Past
+    private Date deletedAt;
+
+    @Column(name = "status")
+    private String status;
+
+    public Subscriber() {}
+
+    public Subscriber(User subscriber, User user, Date dateOfFollowing, Date createdAt, Date updatedAt, Date deletedAt, String status) {
+        this.subscriber = subscriber;
+        this.user = user;
+        this.dateOfFollowing = dateOfFollowing;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.deletedAt = deletedAt;
+        this.status = status;
+    }
+
+    public User getSubscriber() {
+        return subscriber;
+    }
+
+    public void setSubscriber(User subscriber) {
+        this.subscriber = subscriber;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Date getDateOfFollowing() {
+        return dateOfFollowing;
+    }
+
+    public void setDateOfFollowing(Date dateOfFollowing) {
+        this.dateOfFollowing = dateOfFollowing;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Date getDeletedAt() {
+        return deletedAt;
+    }
+
+    public void setDeletedAt(Date deletedAt) {
+        this.deletedAt = deletedAt;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
--- src/main/java/ru/ChillyPeppersInc/koster/models/User.java
+++ src/main/java/ru/ChillyPeppersInc/koster/models/User.java
@@ -1,15 +1,14 @@
 package ru.ChillyPeppersInc.koster.models;
 
 
-import jakarta.persistence.*;
+import  jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
-import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.format.annotation.DateTimeFormat;
-
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name = "Users")
@@ -18,6 +17,14 @@ public class User {
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @OneToMany(mappedBy = "userId")
+    private List<Post> posts;
+
+    @NotEmpty(message = "Username shouldn't be empty")
+    @Size(min = 5, max = 50, message = "Username length should be between 5 and 20")
+    @Column(name = "username", unique = true)
+    private String username;
 
     @NotEmpty(message = "Name shouldn't be empty")
     @Size(min = 2, max = 50, message = "Name length should be between 2 and 50")
@@ -29,15 +36,13 @@ public class User {
     @Column(name = "surname")
     private String surname;
 
-    @NotEmpty(message = "Name shouldn't be empty")
-    @Size(min = 1, max = 50, message = "Name length should be between 1 and 50")
-    @Column(name = "user_name")
-    private String userName;
-
     @Column(name = "email")
-    // @NotEmpty(message = "Email shouldn't be empty")
+    @NotEmpty(message = "Email shouldn't be empty")
     @Email
     private String email;
+
+    @Column(name = "avatar")
+    private String avatar;
 
     @Column(name = "date_of_birthday")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
@@ -45,17 +50,50 @@ public class User {
     @Past
     private Date birthdate;
 
+    @Column(name = "is_active")
+    private boolean isActive;
+
+    @Column(name = "last_login")
+    private Date lastLogin;
+
     @Column(name = "password")
     @NotEmpty(message = "Password shouldn't be empty")
     private String password;
 
-    public User(String name, String surname, String userName, String mail, Date dateOfBirthday, String password) {
+    @Column(name = "created_at")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Past
+    private Date createdAt;
+
+    @Column(name = "updated_at")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Past
+    private Date updatedAt;
+
+    @Column(name = "deleted_at")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Past
+    private Date deletedAt;
+
+    @Column(name = "status")
+    private String status;
+
+
+    public User(List<Post> posts, String username, String name, String surname, String email, String avatar, Date birthdate, boolean isActive, Date lastLogin, String password, Date createdAt, Date updatedAt, Date deletedAt, String status) {
+        this.posts = posts;
+        this.username = username;
         this.name = name;
         this.surname = surname;
-        this.email = mail;
-        this.birthdate = dateOfBirthday;
-        this.userName = userName;
+        this.email = email;
+        this.avatar = avatar;
+        this.birthdate = birthdate;
+        this.isActive = isActive;
+        this.lastLogin = lastLogin;
         setPassword(password);
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.deletedAt = deletedAt;
+        this.status = status;
     }
 
     public User() {
@@ -79,11 +117,11 @@ public class User {
     }
 
     public String getUserName() {
-        return userName;
+        return username;
     }
 
     public void setUserName(String userName) {
-        this.userName = userName;
+        this.username = userName;
     }
 
     public String getEmail() {
@@ -94,7 +132,54 @@ public class User {
         this.email = mail;
     }
 
-    public Date getDateOfBirthday() {
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getPassword() {
+        return password;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Date getDeletedAt() {
+        return deletedAt;
+    }
+
+    public void setDeletedAt(Date deletedAt) {
+        this.deletedAt = deletedAt;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Date getBirthdate() {
         return birthdate;
     }
 
@@ -102,7 +187,19 @@ public class User {
         this.birthdate = birthdate;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public Date getLastLogin() {
+        return lastLogin;
+    }
+
+    public void setLastLogin(Date lastLogin) {
+        this.lastLogin = lastLogin;
     }
 }
