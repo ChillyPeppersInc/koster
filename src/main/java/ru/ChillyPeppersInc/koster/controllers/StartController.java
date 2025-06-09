d260b94964fdd3030c3f55e111236f71e9be74cd
untou4ed <antonkotelnikov1976@gmail.com>
2025-06-09 15:55:31 +0300

    new start controller

--- src/main/java/ru/ChillyPeppersInc/koster/controllers/StartController.java
+++ src/main/java/ru/ChillyPeppersInc/koster/controllers/StartController.java
@@ -1,14 +1,19 @@
 package ru.ChillyPeppersInc.koster.controllers;
 
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.ChillyPeppersInc.koster.models.User;
 
 @Controller
 public class StartController {
 
     @GetMapping("/")
-    public String welcomePage() {
-        return "index.html";
+    public String welcomePage(Model model,
+                              @AuthenticationPrincipal User currentUser) {
+        model.addAttribute("user", currentUser);
+        return "index";
     }
 }
