107c9143e4e59c6b32634985a3abc3d566c142aa
Arslan764 <arslanhismatullin00@mail.ru>
2025-06-14 19:20:06 +0300

    логика создания поста вынесена в сервис

--- src/main/java/ru/ChillyPeppersInc/koster/Services/PostService.java
+++ src/main/java/ru/ChillyPeppersInc/koster/Services/PostService.java
@@ -2,26 +2,47 @@ package ru.ChillyPeppersInc.koster.Services;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 import ru.ChillyPeppersInc.koster.models.Post;
 import ru.ChillyPeppersInc.koster.models.User;
 import ru.ChillyPeppersInc.koster.repositories.PostRepository;
 
+import java.io.IOException;
+import java.sql.Date;
+import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
 public class PostService {
 
     private final PostRepository postRepository;
+    private final FileStorageService fileStorageService;
 
-    public PostService(PostRepository postRepository) {
+    public PostService(PostRepository postRepository, FileStorageService fileStorageService) {
         this.postRepository = postRepository;
+        this.fileStorageService = fileStorageService;}
+
+    @Transactional
+    public Post createPost(User user, String content, String geolocation) {
+        Post post = new Post();
+        post.setUser(user);
+        post.setContent(content);
+        post.setGeolocation(geolocation);
+        post.setCreatedAt(Date.valueOf(LocalDate.now()));
+        post.setUpdatedAt(Date.valueOf(LocalDate.now()));
+        post.setPublic(true);
+        post.setStatus("active");
+        return postRepository.save(post);
     }
 
     @Transactional
-    public Post createPost(Post post) {
-        return postRepository.save(post);
+    public void attachImageToPost(Post post, MultipartFile image) throws IOException {
+        if (image != null && !image.isEmpty()) {
+            String imageUrl = fileStorageService.storeFile(image, post.getId());
+            post.setImage(imageUrl);
+            updatePost(post);
+        }
     }
 
     public List<Post> findAllByUser(User user) {
--- src/main/java/ru/ChillyPeppersInc/koster/controllers/PostController.java
+++ src/main/java/ru/ChillyPeppersInc/koster/controllers/PostController.java
@@ -6,8 +6,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -20,22 +18,18 @@ import ru.ChillyPeppersInc.koster.models.User;
 
 import java.io.IOException;
 import java.security.Principal;
-import java.sql.Date;
-import java.time.LocalDate;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class PostController {
 
     private final PostService postService;
-    private final FileStorageService fileStorageService;
+
     private final UserService userService;
 
     @Autowired
-    public PostController(PostService postService, FileStorageService fileStorageService, UserService userService) {
+    public PostController(PostService postService, UserService userService) {
         this.postService = postService;
-        this.fileStorageService = fileStorageService;
         this.userService = userService;
     }
 
@@ -45,35 +39,16 @@ public class PostController {
             @RequestParam("content") String content,
             @RequestParam(value = "image", required = false) MultipartFile image,
             @RequestParam(value = "geolocation", required = false) String geolocation,
-            HttpServletRequest request,
             HttpServletResponse response) {
 
         String username = principal.getName();
         User currentUser = userService.findByUsername(username).
                 orElseThrow(() -> new UsernameNotFoundException(username));
 
+        Post newPost = postService.createPost(currentUser, content, geolocation);
 
         try {
-            // Создаем пост
-            Post post = new Post();
-            post.setUser(currentUser);
-            post.setContent(content);
-            post.setGeolocation(geolocation);
-            post.setCreatedAt(Date.valueOf(LocalDate.now()));
-            post.setUpdatedAt(Date.valueOf(LocalDate.now()));
-            post.setPublic(true); // или можно передавать параметром
-            post.setStatus("active");
-            // Сохраняем пост, чтобы получить ID
-            Post savedPost = postService.createPost(post);
-
-            // Если есть изображение, сохраняем его и обновляем пост
-            if (image != null && !image.isEmpty()) {
-                String imageUrl = fileStorageService.storeFile(image, savedPost.getId());
-                savedPost.setImage(imageUrl);
-                postService.updatePost(savedPost);
-            }
-
-
+            postService.attachImageToPost(newPost, image);
             response.sendRedirect("/profile");
             return null;
         } catch (IOException e) {
