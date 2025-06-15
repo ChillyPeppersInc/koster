fe5073024016b9439eea9e1e0d263e85096cb5d0
untou4ed <antonkotelnikov1976@gmail.com>
2025-06-15 22:55:02 +0300

    user + post are back eeeeeeeeeee

---
+++ src/main/java/ru/ChillyPeppersInc/koster/models/Post.java
@@ -0,0 +1,144 @@
+package ru.ChillyPeppersInc.koster.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Past;
+import org.springframework.format.annotation.DateTimeFormat;
+import java.sql.Date;
+
+@Entity
+@Table(name = "Posts")
+public class Post {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @JoinColumn(name = "user_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "image")
+    private String image;
+
+    @Column(name = "geolocation")
+    private String geolocation;
+
+    @Column(name = "created_at")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date createdAt;
+
+    @Column(name = "updated_at")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Past
+    private Date updatedAt;
+
+    @Column(name = "deleted_at")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Past
+    private Date deletedAt;
+
+    @Column(name = "is_public")
+    private boolean isPublic;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "likes")
+    private int likes;
+
+    public Post() {}
+
+    public Post(User user, String content, String image, String geolocation, Date createdAt, Date updatedAt, Date deletedAt, boolean isPublic, String status) {
+        this.user = user;
+        this.content = content;
+        this.image = image;
+        this.geolocation = geolocation;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.deletedAt = deletedAt;
+        this.isPublic = isPublic;
+        this.status = status;
+        this.likes = 0;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Date getDeletedAt() {
+        return deletedAt;
+    }
+
+    public void setDeletedAt(Date deletedAt) {
+        this.deletedAt = deletedAt;
+    }
+
+    public boolean isPublic() {
+        return isPublic;
+    }
+
+    public void setPublic(boolean aPublic) {
+        isPublic = aPublic;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getGeolocation() {
+        return geolocation;
+    }
+
+    public void setGeolocation(String geolocation) {
+        this.geolocation = geolocation;
+    }
+
+    public int getId() {return id;}
+
+    public int getLikes() {return likes;}
+
+    public void setLikes(int likes) {this.likes = likes;}
+}
