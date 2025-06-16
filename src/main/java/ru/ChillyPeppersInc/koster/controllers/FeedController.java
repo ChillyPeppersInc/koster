85c19ad7ffe6641c35ec718faf7e75a0a5c94ea0
untou4ed <antonkotelnikov1976@gmail.com>
2025-06-16 15:51:41 +0300

    feed fix

--- src/main/java/ru/ChillyPeppersInc/koster/controllers/FeedController.java
+++ src/main/java/ru/ChillyPeppersInc/koster/controllers/FeedController.java
@@ -6,12 +6,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import ru.ChillyPeppersInc.koster.Services.PostService;
+import ru.ChillyPeppersInc.koster.models.Comment;
 import ru.ChillyPeppersInc.koster.models.Post;
 import ru.ChillyPeppersInc.koster.models.User;
 import ru.ChillyPeppersInc.koster.repositories.PostRepository;
 import ru.ChillyPeppersInc.koster.repositories.UsersRepository;
 
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -30,9 +33,11 @@ public class FeedController {
     public String feedPage(Model model, Principal principal) {
         List<Post> allPosts = postService.getAll();
         allPosts.stream().distinct().collect(Collectors.toList());
+        List<Post> sortedPosts =  allPosts.stream()
+                .sorted(Comparator.comparing(Post::getCreatedAt)).collect(Collectors.toList());
         User user = usersRepository.findByUsername(principal.getName()).
                 orElseThrow(() -> new UsernameNotFoundException("User not found"));
-        model.addAttribute("posts", allPosts.reversed());
+        model.addAttribute("posts", sortedPosts.reversed());
         model.addAttribute("user", user);
         return "feed";
     }
